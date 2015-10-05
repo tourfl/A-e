466c487f5010caf476b5af66a3c2101f209b76e9
zeggais <said.zeggai@gmail.com>
2015-10-05 17:32:19 +0200

    Update emulARM.c

--- code/src/emulARM.c
+++ code/src/emulARM.c
@@ -4,13 +4,54 @@
  * @brief Main pour le début du projet émulateur.
  *
  */
- 
-#include "common/interpreteur.h"
 
-/**
- * allocation et init interpreteur
- * @return un pointeur vers une structure allouée dynamiquement
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* la librairie readline */
+#include <readline/readline.h>
+#include <readline/history.h>
+
+/* macros de DEBUG_MSG fournies , etc */
+#include "common/notify.h"
+
+
+/* prompt du mode shell interactif */
+#define PROMPT_STRING "ARMShell : > "
+
+/* taille max pour nos chaines de char */
+#define MAX_STR 1024
+
+
+/*************************************************************\
+Valeur de retour speciales pour la fonction
+	int execute_cmd(interpreteur inter) ;
+Toute autre valeur signifie qu'une erreur est survenue
+ \*************************************************************/
+#define CMD_OK_RETURN_VALUE 0
+#define CMD_EXIT_RETURN_VALUE -1
+#define CMD_UNKOWN_RETURN_VALUE -2
+
+
+
+enum {HEXA,OCT,DEC,UNKNOWN};
+
+/* mode d'interaction avec l'interpreteur (exemple)*/
+typedef enum {INTERACTIF,SCRIPT,DEBUG_MODE} inter_mode;
+
+/* structure passée en parametre qui contient la connaissance de l'état de
+ * l'interpréteur
  */
+typedef struct {
+    inter_mode mode;
+    char input[MAX_STR];
+    char * from;
+    char first_token;
+} *interpreteur;
+
+
+
 interpreteur init_inter(void) {
     interpreteur inter = calloc(1,sizeof(*inter));
     if (inter ==NULL)
@@ -18,23 +59,60 @@ interpreteur init_inter(void) {
     return inter;
 }
 
-/**
- * desallocation de l'interpreteur
- * @param inter le pointeur vers l'interpreteur à libérer
- */
-void del_inter(interpreteur inter) {
-    if (inter !=NULL)
-        free(inter);
+
+
+int get_type(char* chaine) {
+    if (is_hexa(chaine))
+        return HEXA;
+    else if (is_oct (chaine))
+	return OCT;
+    else if (is_dec (chaine))
+	return DEC;	
+    else return UNKNOWN;
 }
 
-/**
- * return le prochain token de la chaine actuellement
- * analysée par l'interpreteur
- * La fonction utilise une variable interne de l'interpreteur
- * pour gérer les premiers appels a strtok
- * @inter le pointeur vers l'interpreteur
- * @return un pointeur vers le token ou NULL
- */
+
+
+
+int is_hexa(char* chaine) {
+    int i;
+    long l;
+    char* p;
+    if (chaine!=NULL && strlen(chaine)>2&& chaine[0]=='0' && chaine[1]=='x' && sscanf(chaine,"%x",&i)==1){
+	 l = strtol (chaine, &p , 16);
+	 if (l!=0) return 1;
+	 else return 0;
+}
+    else return 0;
+}
+
+int is_oct(char* chaine) {
+    int i;
+    long l;
+    char* p;
+    if (chaine!=NULL && strlen(chaine)> 1 && chaine[0]=='0' && sscanf(chaine,"%x",&i)==1){
+	 l = strtol (chaine, &p , 8);
+	 if (l!=0) return 1;
+	 else return 0;
+}
+    else return 0;
+}
+
+int is_dec (char* chaine){
+    int i;
+    long l;
+    char* p;
+    if (chaine!=NULL && chaine[0] != '0' && sscanf(chaine,"%x",&i)==1){
+	 l = strtol (chaine, &p , 10);
+	 if (l!=0) return 1;
+	 else return 0;
+}
+    else return 0;
+}   
+
+
+
+
 char* get_next_token(interpreteur inter) {
 
     char       *token = NULL;
@@ -55,128 +133,11 @@ char* get_next_token(interpreteur inter) {
     return token;
 }
 
-/**
- * teste si un token est une valeur hexa
- * ATTENTION cette méthode n'est pas complete et ne fonctionnera pas dans tous les cas
- * essayer avec 0x567ZRT...
- *@param chaine le token à analyser
- *@return 0 si non-hexa, non null autrement
- */
-int is_hexa(char* chaine) {
-    int i;
-    return (chaine!=NULL && strlen(chaine)>2&& chaine[0]=='0' && chaine[1]=='x' && sscanf(chaine,"%x",&i)==1);
-}
-
-/**
- * retourne le type du token (fonction très incomplete)
- * @param chaine le token à analyser
- * @return un entier correspondant au type du token
- *
- */
-int get_type(char* chaine) {
-    if (is_hexa(chaine))
-        return HEXA;
-    return UNKNOWN;
-}
-
-/*************************************************************\
-Les commandes de l'émulateur.
-
- Dans cette version, deux commandes :
-	"test" qui attend un nombre strictement positifs d'hexa strictement positifs et affiche ce(s) nombre() +1 dans le terminal
-	"exit" qui quitte l'émulateur
-
- \*************************************************************/
-
-/**
- * version de la commande test qui prend en entrée un hexa
- * la fonction verifie si le nombre est >=0
- * @param hexValue la valeur à afficher
- * @return 0 en case de succes, un nombre positif sinon
- */
-int _testcmd(int hexValue) {
-    if(hexValue <= 0) {
-        WARNING_MSG("command %s  - invalid call. The argument should be positive.\n", "test");
-        return 2;
-    }
-    fprintf(stdout, "CMD TEST RESULT 0x%x\n", hexValue + 1);
-
-    return CMD_OK_RETURN_VALUE;
-}
 
 
-/**
- * version de la commande test qui analyse la chaîne entrée à
- * l'interpreteur.
- * Si la commande est correcte elle est executée.
- * Si la commande contient plusieurs parametres valides, elle
- * est excutée plusieurs fois.
- * @param inter l'interpreteur qui demande l'analyse
- * @return 0 en case de succes, un nombre positif sinon
- */
-
-int testcmd(interpreteur inter) {
-    DEBUG_MSG("Chaine : %s", inter->input);
-    int return_value=0;
-
-    int no_args=1;
-    /* la commande test attend un argument hexa */
-    int hexValue;
-    char * token=NULL;
-
-    /* la boucle permet d'executé la commande de manière recurrente*/
-    /* i.e., tant qu'il y a un hexa on continue*/
-    while((token = get_next_token(inter))!=NULL && return_value==0) {
-        no_args=0;
-        switch(get_type(token)) {
-        case HEXA:
-            sscanf(token,"%x",&hexValue);
-            return_value = _testcmd(hexValue);
-            break;
-        default :
-            WARNING_MSG("value %s is not a valid argument of command %s\n",token,"testcmd");
-            return 1;
-        }
-    }
 
 
-    if(no_args) {
-        WARNING_MSG("no argument given to command %s\n","testcmd");
-        return 1;
-    }
 
-
-    return return_value;
-}
-
-
-/**
- * commande exit qui ne necessite pas d'analyse syntaxique
- * @param inter l'interpreteur qui demande l'analyse
- * @return 0 en case de succes, un nombre positif sinon
- */
-int exitcmd(interpreteur inter) {
-    INFO_MSG("Bye bye !");
-    return CMD_EXIT_RETURN_VALUE;
-}
-
-
-/*************************************************************\
- Les deux fonctions principales de l'émulateur.
-	execute_cmd: parse la commande et l'execute en appelant la bonne fonction C
-	acquire_line : recupere une ligne (donc une "commande") dans le flux
- \*************************************************************/
-
-
-/**
-*
-* @brief parse la chaine courante de l'interpreteur à la recherche d'une commande, et execute cette commande.
-* @param inter l'interpreteur qui demande l'analyse
-* @return CMD_OK_RETURN_VALUE si la commande s'est exécutée avec succès (0)
-* @return CMD_EXIT_RETURN_VALUE si c'est la commande exit. Dans ce cas, le programme doit se terminer. (-1)
-* @return CMD_UNKOWN_RETURN_VALUE si la commande n'est pas reconnue. (-2)
-* @return tout autre nombre (eg tout nombre positif) si erreur d'execution de la commande
-*/
 int execute_cmd(interpreteur inter) {
     DEBUG_MSG("input '%s'", inter->input);
     char cmdStr[MAX_STR];
@@ -196,11 +157,17 @@ int execute_cmd(interpreteur inter) {
     if(strcmp(token, "exit") == 0) {
         return exitcmd(inter);
     }
-    else if(strcmp(token, "test") == 0) {
-        return testcmd(inter);
-    }
     else if(strcmp(token, "load") == 0) {
         return loadcmd(inter);
+    }
+    else if(strcmp(token, "disp") == 0) {
+	return dispcmd(inter);
+    }
+    else if(strcmp(token, "disasm") == 0) {
+	return disasmcmd(inter);
+    }
+    else if(strcmp(token, "set") == 0) {
+	return setcmd(inter);
     }
 
     WARNING_MSG("Unknown Command : '%s'\n", cmdStr);
@@ -211,56 +178,8 @@ int execute_cmd(interpreteur inter) {
 
 
 
-/**
- * @param in Input line (possibly very badly written).
- * @param out Line in a suitable form for further analysis.
- * @return nothing
- * @brief This function will prepare a line for further analysis.
- *
- * This function will prepare a line for further analysis and check for low-level syntax errors.
- * colon, brackets, commas are surrounded with blank; tabs are replaced with blanks.
- * negs '-' are attached to the following token (e.g., "toto -   56" -> "toto -56")  .
- */
-void string_standardise( char* in, char* out ) {
-    unsigned int i=0, j;
-
-    for ( j= 0; i< strlen(in); i++ ) {
-
-        /* insert blanks around special characters*/
-        if (in[i]==':' || in[i]=='+' || in[i]=='~') {
-            out[j++]=' ';
-            out[j++]=in[i];
-            out[j++]=' ';
-
-        }
-
-        /* remove blanks after negation*/
-        else if (in[i]=='-') {
-            out[j++]=' ';
-            out[j++]=in[i];
-            while (isblank((int) in[i+1])) i++;
-        }
-
-        /* insert one blank before comments */
-        else if (in[i]=='#') {
-            out[j++]=' ';
-            out[j++]=in[i];
-        }
-        /* translate tabs into white spaces*/
-        else if (isblank((int) in[i])) out[j++]=' ';
-        else out[j++]=in[i];
-    }
-}
 
 
-/**
- *
- * @brief extrait la prochaine ligne du flux fp.
- * Si fp ==stdin, utilise la librairie readline pour gestion d'historique de commande.
- *
- * @return 0 si succes.
- * @return un nombre non nul si aucune ligne lue
- */
 int  acquire_line(FILE *fp, interpreteur inter) {
     char* chunk =NULL;
 
@@ -308,87 +227,12 @@ int  acquire_line(FILE *fp, interpreteur inter) {
 }
 
 
-/****************/
-void usage_ERROR_MSG( char *command ) {
-    fprintf( stderr, "Usage: %s [file.emul]\n   If no file is given, executes in Shell mode.", command );
+
+
+
+int exitcmd(interpreteur inter) {
+    INFO_MSG("Bye bye !");
+    return CMD_EXIT_RETURN_VALUE;
 }
 
 
-/**
- * Programme principal
- */
-int main ( int argc, char *argv[] ) {
-    /* exemples d'utilisation des macros du fichier notify.h */
-    INFO_MSG("Un message INFO_MSG : Debut du programme %s", argv[0]); /* macro INFO_MSG */
-    WARNING_MSG("Un message WARNING_MSG !"); /* macro INFO_MSG */
-    DEBUG_MSG("Un message DEBUG_MSG !"); /* macro DEBUG_MSG : uniquement si compil en mode DEBUG_MSG */
-    interpreteur inter=init_inter(); /* structure gardant les infos et états de l'interpreteur*/
-    FILE *fp = NULL; /* le flux dans lequel les commande seront lues : stdin (mode shell) ou un fichier */
-
-    if ( argc > 2 ) {
-        usage_ERROR_MSG( argv[0] );
-        exit( EXIT_FAILURE );
-    }
-    if(argc == 2 && strcmp(argv[1], "-h") == 0) {
-        usage_ERROR_MSG( argv[0] );
-        exit( EXIT_SUCCESS );
-    }
-
-    /*par defaut : mode shell interactif */
-    fp = stdin;
-    inter->mode = INTERACTIF;
-    if(argc == 2) {
-        /* mode fichier de commandes */
-        fp = fopen( argv[1], "r" );
-        if ( fp == NULL ) {
-            perror( "fopen" );
-            exit( EXIT_FAILURE );
-        }
-        inter->mode = SCRIPT;
-    }
-
-    /* boucle infinie : lit puis execute une cmd en boucle */
-    while ( 1 ) {
-
-
-
-        if (acquire_line( fp,  inter)  == 0 ) {
-            /* Une nouvelle ligne a ete acquise dans le flux fp*/
-
-            int res = execute_cmd(inter); /* execution de la commande */
-
-            // traitement des erreurs
-            switch(res) {
-            case CMD_OK_RETURN_VALUE:
-                break;
-            case CMD_EXIT_RETURN_VALUE:
-                /* sortie propre du programme */
-                if ( inter->mode == SCRIPT ) {
-                    fclose( fp );
-                }
-                del_inter(inter);
-                exit(EXIT_SUCCESS);
-                break;
-            default:
-                /* erreur durant l'execution de la commande */
-                /* En mode "fichier" toute erreur implique la fin du programme ! */
-                if (inter->mode == SCRIPT) {
-                    fclose( fp );
-                    del_inter(inter);
-                    /*macro ERROR_MSG : message d'erreur puis fin de programme ! */
-                    ERROR_MSG("ERREUR DETECTEE. Aborts");
-                }
-                break;
-            }
-        }
-        if( inter->mode == SCRIPT && feof(fp) ) {
-            /* mode fichier, fin de fichier => sortie propre du programme */
-            DEBUG_MSG("FIN DE FICHIER");
-            fclose( fp );
-            del_inter(inter);
-            exit(EXIT_SUCCESS);
-        }
-    }
-    /* tous les cas de sortie du programme sont gérés plus haut*/
-    ERROR_MSG("SHOULD NEVER BE HERE\n");
-}
