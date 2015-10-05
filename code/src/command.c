25095947d0cf24744200e382562e521b508b52f2
zeggais <said.zeggai@gmail.com>
2015-10-05 21:22:59 +0200

    Update command.c

--- code/src/command.c
+++ code/src/command.c
@@ -149,20 +149,20 @@ registre wich_reg (char*nom) {
 			return r[i];
 		}
 	}
-	if (strcmp(r.name, sp) == 1) {
+	if (strcmp(r.name, sp) == 0) {
 		return sp;
 	}
-	else if (strcmp(r.name, lr) == 1){
+	else if (strcmp(r.name, lr) == 0){
 		return lr;
 	}
-	else if (strcmp(r.name, pc) == 1) {
+	else if (strcmp(r.name, pc) == 0) {
 		return pc;
 	}
-	else if (strcmp(r.name, aspr) == 1) {
+	else if (strcmp(r.name, aspr) == 0) {
 		return aspr;
 	}
 
-	else return 0;
+	else return NULL;
 	
 }
 
@@ -250,7 +250,7 @@ int setcmd (interpreteur inter){
 		char* reg = NULL;
 		reg = get_netxt_token (inter);
 		valeur = get_next_token(inter);
-		a = set_reg(inter,reg,valeur); //fonction qui copie "valeur" dans le registre "registre"; 
+		a = set_reg(inter,reg,valeur); //fonction qui copie "valeur" dans le registre "reg"; 
 		return (a); 
 		
 	}
