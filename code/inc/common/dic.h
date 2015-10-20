31170f3eeb0c6aceb65663626735fc342f843ae3
Dein <raphaeldein@gmail.com>
2015-10-20 23:19:57 +0200

    ajout du fichier dictionnaire

---
+++ code/inc/common/dic.h
@@ -0,0 +1,15 @@
+
+#ifndef  DIC_H
+#define DIC_H
+
+#include <stdio.h>
+
+#define TAILLE_MAX 1000
+
+int disp_dic();
+
+int comp_instr(); // TODO
+
+int bin_x_bin(unsigned long bin_1, unsigned long bin_2); // TODO
+
+#endif
---
+++ code/src/dic/dic.c
@@ -0,0 +1,20 @@
+#include "common/dic.h"
+
+int disp_dic()
+{
+	FILE *dic = NULL;
+	char chaine[TAILLE_MAX] = "";
+
+	dic = fopen("lib/instructions.dic", "r");
+
+	if(dic == NULL) return 1;
+
+	while (fgets(chaine, TAILLE_MAX, dic) != NULL)
+	{
+		printf("%s", chaine);
+	}
+
+	fclose(dic);
+
+	return 0;
+}
