c36e7aca20ae30a2412496f2c96e56a3c2856c4f
Dein <raphaeldein@gmail.com>
2015-11-12 18:31:07 +0100

    disasm OK

---
+++ code/src/notify.c
@@ -0,0 +1,68 @@
+#include "com/notify.h"
+
+
+
+
+
+
+/*
+ * Fonction qui interprète les retours des fonctions
+ *
+ */
+
+
+void which_error(int code)
+{
+	DEBUG_MSG("Entering function which_error");
+
+	switch (code) {
+		case 0:
+			DEBUG_MSG("No Problem");
+			break;
+
+		case 1:
+		{
+			DEBUG_MSG("Chaine vide");
+			break;
+		}
+
+		case 2:
+		{
+			DEBUG_MSG("Error with malloc");
+			break;
+		}
+
+		case 3:
+		{
+			DEBUG_MSG("Error with size");
+			break;
+		}
+
+		case 5:
+		{
+			DEBUG_MSG("Error with strcpy");
+			break;
+		}
+
+		case 6:
+		{
+			DEBUG_MSG("Problem with strcat");
+			break;
+		}
+
+		case 8:
+		{
+			DEBUG_MSG("Error with fseek");
+			break;
+		}
+		default:
+		{
+			DEBUG_MSG("Untracked error");
+			break;
+		}
+	}
+
+
+	DEBUG_MSG("Exiting function which_error");
+}
+
