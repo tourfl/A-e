9872d78710739805578fbab501968c91629b3783
zeggais <said.zeggai@gmail.com>
2015-12-12 19:15:55 +0100

    Update ins.h

--- code/inc/dic/ins.h
+++ code/inc/dic/ins.h
@@ -31,9 +31,9 @@ int sub_imm_T3(Instruction ins,  int* setflags , int* n, int* d);
 int sub_imm_T4(Instruction ins,  int* setflags, int* n, int* d );
 
 int mov_imm (Instruction ins, Emulator* emul);
-int mov_imm_T1 (Instruction ins, Emulator* emul, int* setflags, int* imm32, int* carry);
-int mov_imm_T2 (Instruction ins, int* setflags, int* imm32, int* carry, int* d);
-int mov_imm_T3 (Instruction ins, int* setflags, int* imm32, int* d);
+int mov_imm_T1 (Instruction ins, Emulator* emul, int* setflags, long* imm32, int* carry);
+int mov_imm_T2 (Instruction ins, int* setflags, long* imm32, int* carry, int* d);
+int mov_imm_T3 (Instruction ins, int* setflags, long* imm32, int* d);
 
 int mov_reg (Instruction ins, Emulator* emul);
 int mov_reg_T1 (Instruction ins, int* setflags,int* d);
@@ -46,16 +46,16 @@ int pop_T2 (Instruction ins, long* registers);
 int pop_T3 (Instruction ins , long* registers , int* t);
 
 int ldr_imm (Instruction ins, Emulator* emul);
-int ldr_imm_T1 (Instruction ins, int* imm32, int* index, int* add, int* wback, int* n, int* t);
-int ldr_imm_T2 (Instruction ins, int* imm32, int* index, int* add, int* wback, int* n, int* t);
-int ldr_imm_T3 (Instruction ins, int* imm32, int* index, int* add, int* wback, int* n, int* t);
+int ldr_imm_T1 (Instruction ins, long* imm32, int* index, int* add, int* wback, int* n, int* t);
+int ldr_imm_T2 (Instruction ins, long* imm32, int* index, int* add, int* wback, int* n, int* t);
+int ldr_imm_T3 (Instruction ins, long* imm32, int* index, int* add, int* wback, int* n, int* t);
 
 int push (Instruction ins, Emulator* emul);
 int push_T1 (Instruction ins, long* registers);
 int push_T2 (Instruction ins, long* registers);
 int push_T3 (Instruction ins, long* registers , int* t);
 
-int ldr_lit (Instruction ins, Emulator* emul);
+int ldr_litt (Instruction ins, Emulator* emul);
 int ldr_litt_T1 (Instruction ins,long* imm32, int* add);
 int ldr_litt_T2 (Instruction ins,long* imm32, int* t, int* add);
 
@@ -64,6 +64,22 @@ int str_imm_T1 (Instruction ins, long* imm32, int* index, int* add, int* wback, 
 int str_imm_T2 (Instruction ins, long* imm32, int* index, int* add, int* wback, int* n );
 int str_imm_T3 (Instruction ins, int* index, int* add, int* wback, int* n, int* t );
 
+int add_reg (Instruction ins, Emulator* emul);
+int add_reg_T1(Instruction ins,  int* setflags /*, int* shift_n, int* shift_t*/) ;
+int add_reg_T2(Instruction ins,  int* setflags,/* int* shift_n, int* shift_t,*/ int* n ,int* d ,int* m);
+int add_reg_T3(Instruction ins, int* setflags, /*int* shift_n, int* shift_t,*/ int* n , int* d, int* m, long* shifted);
 
+int sub_reg (Instruction ins, Emulator* emul);
+int sub_reg_T1(Instruction ins,  int* setflags);
+int sub_reg_T2(Instruction ins,  int* setflags, int* n ,int* d ,int* m, long* shifted);
+
+int cmp_reg (Instruction ins, Emulator* emul);
+int cmp_reg_T1(Instruction ins);
+int cmp_reg_T2(Instruction ins, int* n, int* m);
+int cmp_reg_T3(Instruction ins, int* n, int* m, long* shifted );
+
+int str_reg (Instruction ins, Emulator* emul);
+int str_reg_T1 (Instruction ins,int* index,int* add,int* wback );
+int str_reg_T2 (Instruction ins, int* index,int* add, int* wback, int* t, int* n, int* m, long* offset );
 
 #endif
