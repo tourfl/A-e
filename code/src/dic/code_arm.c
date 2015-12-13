22cc6f53b14ee4d580dcbb7abfae3c448e88cd6d
zeggais <said.zeggai@gmail.com>
2015-12-13 20:39:14 +0100

    Update code_arm.c

--- code/src/dic/code_arm.c
+++ code/src/dic/code_arm.c
@@ -77,9 +77,15 @@ void SignExtend_plgtab (Plgtab *imm)
 
 void BranchWritePC (vaddr32 adresse, Emulator *emul) {
 	
-	adresse = adresse & ~(1u << 0) ;
-	// PC = adresse;
-
+	//adresse = adresse & ~(1u << 0) ;
+	if(adresse & (1u << 1) ) {
+		emul->reg[15] = adresse  ;
+	}
+	
+	else {
+		emul->reg[15]= adresse - 2;
+	}
+	printf ("pc après branchement : %lx\n" , emul->reg[15]);
 }
 
 
@@ -90,82 +96,31 @@ void BXWritePC (vaddr32 a, Emulator *emul) {
 
 }
 
-
-int SInt16 (int x ) {
-
-	int result = 0;
-	int i;
-	for (i = 0; i<=15; i++) {
-		if (x & (1u << i ) ){ 
-			result = result + 2^i;
-		}
-	}
-	if (x & (1u << 15) ) {
-		result = result - 2^15;
-	}
-	return result;
-}
-
-long SInt32 (long x) {
-	
-	long result = 0;
-	int i;
-	for (i = 0; i<=31; i++) {
-		if (x & (1u << i ) ){ 
-			result = result + 2^i;
-		}
-	}
-	if (x & (1u << 31) ) {
-		result = result - 2^32;
-	}
-	return result;
-}
-
-
-int UInt16 (int x) {
-	
-	int result = 0;
-	int i;
-	for (i = 0 ; i<=15 ; i++) { 
-		if (x & (1u << i)) {
-			result = result + 2^i;
-		}
-	}
-	return result;
-}
-
-long UInt32 (long x) {
-	
-	long result = 0;
-	int i;
-	for (i = 0 ; i<=31 ; i++) { 
-		if (x & (1u << i) ){
-			result = result + 2^i;
-		}
-	}
-	return result;
-}
-
-unsigned long AddWithCarry (unsigned long registre , unsigned long imm32 , int* carry , int* overflow)
-{
+long AddWithCarry (long registre , long imm32 , int* carry , int* overflow) {
 
 
 	unsigned long unsigned_sum;
 	long signed_sum;
 	long result;
-	long mask;
-	mask = 0x0000FFFF;
-	
-	unsigned_sum = UInt32 (registre) + UInt32 (imm32) + UInt16 (*carry);
-    	signed_sum   = SInt32 (registre) + SInt32 (imm32) + UInt16 (*carry);
-    	result       = unsigned_sum & mask ;  // same value as signed_sum<N-1:0>
-    	if (UInt32 (result) == unsigned_sum ) {
+	unsigned long uregistre , uimm32;
+	uregistre = (unsigned long) registre;
+	uimm32 = (unsigned long ) imm32;
+	/*if (emul->reg[16] & (1u << 29)) {
+		unsigned_sum = UInt32 (registre) + UInt32 (imm32) + 1;
+	    	signed_sum   = SInt32 (registre) + SInt32 (imm32) + 1;
+	}
+	else {*/
+		unsigned_sum = uregistre + uimm32 ;
+	    	signed_sum   = registre + imm32 ;
+	//}		
+    	result = signed_sum  ;
+	if (result == unsigned_sum ) {
 		*carry = 0; 
 	}
 	else {
 		*carry = 1;
 	}
-    	if (SInt32 (result) == signed_sum ) {
+    	if (result == signed_sum ) {
 		*overflow = 0;
 	}	
 	else {
@@ -206,4 +161,144 @@ int BitCount (long a) {
 		}
 	}
 	return j;
+}
+
+
+int condition (int cond, Emulator* emul) {	
+
+	if (cond == 0) {
+		if (emul->reg[16] & (1u << 30) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (cond == 1) {
+		if (emul->reg[16] & (1u << 30) ) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+  	}
+
+
+
+
+	else if (cond == 2) {
+		if (emul->reg[16] & (1u << 29) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (cond == 3) {
+		if (emul->reg[16] & (1u << 29) ) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+
+
+
+	else if (cond == 4) {
+		if (emul->reg[16] & (1u << 31) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (cond == 5) {
+		if (emul->reg[16] & (1u << 31) ) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+
+
+
+	else if (cond == 6) {
+		if (emul->reg[16] & (1u << 28) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (cond == 7) {
+		if (emul->reg[16] & (1u << 28) ) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+
+
+
+	else if (cond == 8) {
+		if ((emul->reg[16] & (1u << 29) ) && !(emul->reg[16] & (1u << 30) ) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (cond == 9) {
+		if (!(emul->reg[16] & (1u << 29) ) || (emul->reg[16] & (1u << 30) ) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+
+	}
+
+
+
+
+	else if (cond == 10) {
+		if ((emul->reg[16] & (1u << 31) ) == (emul->reg[16] & (1u << 28) ) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (cond == 11) {
+		if ((emul->reg[16] & (1u << 31) ) == (emul->reg[16] & (1u << 28) ) ) {
+			return 0;
+		}
+		else {
+			return 1;
+		}
+	}
+
+
+
+	else if (cond == 12) {
+		if (!emul->reg[16] && (emul->reg[16] & (1u << 31) ) == (emul->reg[16] & (1u << 28) ) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else if (cond == 13) {
+		if (emul->reg[16] || (emul->reg[16] & (1u << 31) ) != (emul->reg[16] & (1u << 28) ) ) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+	else 
+		return 0;
 }
